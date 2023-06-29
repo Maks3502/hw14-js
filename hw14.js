3aa7239fcac71177d50299b6977f1aeb20239fb7
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-06-29 18:41:19 +0300

    Add files via upload

---
+++ hw14.js
@@ -0,0 +1,134 @@
+// завдання1
+
+const myArray = {
+
+}
+
+myArray.name = "Max";
+myArray.age = 14;
+myArray.city = "Nikopol";
+myArray.ocupation = "Football"
+
+console.log(myArray.countre);
+
+
+console.log(myArray.name)
+
+
+// завдання2
+
+const purchase = {
+    
+sugar:{
+    price: 40,
+    crowbar: "1кг",
+    package: "white",
+    meda: "Poland"
+},
+
+wine:{
+    price:120,
+    made: "Italy",
+    package: "red"
+},
+bread:{
+price: 22,
+made: "Ukrain"
+}
+}
+
+
+console.log(purchase);
+
+// завдання3
+
+// 1
+
+// let apartment;
+
+// apartment = {
+    // tags  :  ["premium", "promoted", "top"],
+//     descr : "Spacious apartment in the city center",
+//     rating  : 4, 
+//     price  : 2153,
+//     imgUrl : "https://via.placeholder.com/640x480",
+//    
+// };
+
+// 2
+// apartment.owner = {
+
+//     name : "Henry",
+
+//     phone  : "982-126-1588",
+
+//     email : "henry.carter@aptmail.com",
+
+// };
+
+
+// 3
+
+// let aptRating = apartment.rating;
+
+
+// let aptDescr  = apartment.descr;
+
+
+// let aptPrice = apartment.price;
+
+
+// let aptTags  = apartment.tags;
+
+// 4
+
+// const ownerName  = apartment.owner.name;
+
+// const ownerPhone = apartment.owner.phone;
+
+// const numberOfTags =  apartment.tags.length;
+
+// const ownerEmail = apartment.owner.email;
+
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+
+// const firstTag = apartment.tags[0];
+
+
+// 5
+// aptTags = apartment["tags"];
+
+// aptRating = apartment["rating"];
+
+// aptPrice = apartment["price"];
+
+// aptDescr = apartment["descr"];
+
+
+// 6
+
+
+// apartment.rating = 4.7;
+
+// apartment.price = 5000;
+
+
+// apartment.owner.name = "Henry Sibola";
+
+// apartment.tags.push("trusted");
+
+
+// console.log(apartment.rating);
+
+// console.log(apartment.tags);
+
+// 7
+
+
+
+
+
+
+
+
+
